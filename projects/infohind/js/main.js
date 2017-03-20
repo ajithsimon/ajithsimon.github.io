3ed71a24b69df50fcf7afd35aa8e4cbce233a24f
Ajithkumar <ajith@gympik.com>
2017-03-20 15:16:28 +0530

    menu hover only for mobile

--- projects/infohind/js/main.js
+++ projects/infohind/js/main.js
@@ -3,8 +3,18 @@
  * Author: Ajithkumar
  * url: ajithsimon.com
  */
-$('ul.nav li.dropdown').hover(function () {
-    $(this).find('.dropdown-menu').stop(true, true).fadeIn(200);
-}, function () {
-    $(this).find('.dropdown-menu').stop(true, true).fadeOut(200);
-});
+function isMobile() {
+    var isMobile = window.matchMedia("only screen and (max-width: 760px)");
+    if (isMobile.matches) {
+        return true;
+    } else {
+        return false;
+    }
+}
+if (!isMobile()){
+    $('ul.nav li.dropdown').hover(function () {
+        $(this).find('.dropdown-menu').stop(true, true).fadeIn(200);
+    }, function () {
+        $(this).find('.dropdown-menu').stop(true, true).fadeOut(200);
+    });
+}
