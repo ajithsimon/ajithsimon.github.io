5d5ca6797530685160b54cf82f2bfaf2fe43499c
Ajithkumar <ajith@gympik.com>
2017-03-22 18:10:29 +0530

    datepicker added field size reduced

--- projects/appraam/assets/js/app-form-elements.js
+++ projects/appraam/assets/js/app-form-elements.js
@@ -1,29 +1,29 @@
 var App = (function () {
-  'use strict';
+    'use strict';
 
-  App.formElements = function( ){
+    App.formElements = function ( ) {
 
-    //Js Code
-    $(".datetimepicker").datetimepicker({
-    	autoclose: true,
-    	componentIcon: '.mdi.mdi-calendar',
-    	navIcons:{
-    		rightIcon: 'mdi mdi-chevron-right',
-    		leftIcon: 'mdi mdi-chevron-left'
-    	}
-    });
-    
-    //Select2
-    $(".mines-select").select2({
-      width: '100%'
-    });
-    //Select2 tags
-    $(".tags").select2({tags: true, width: '100%'});
+        //Js Code
+        $(".datetimepicker").datetimepicker({
+            autoclose: true,
+            componentIcon: '.mdi.mdi-calendar',
+            navIcons: {
+                rightIcon: 'mdi mdi-chevron-right',
+                leftIcon: 'mdi mdi-chevron-left'
+            }
+        });
+$(".date-widget").datetimepicker();
+        //Select2
+        $(".mines-select").select2({
+            width: '100%'
+        });
+        //Select2 tags
+        $(".tags").select2({tags: true, width: '100%'});
 
-    //Bootstrap Slider
-    $('.bslider').bootstrapSlider();
-    
-  };
+        //Bootstrap Slider
+        $('.bslider').bootstrapSlider();
 
-  return App;
+    };
+
+    return App;
 })(App || {});
